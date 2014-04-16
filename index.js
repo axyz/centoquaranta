935ca3bdf9ef238855b2e333f7b8c03155dd1541
Andrea Moretti <axyzxp@gmail.com>
2014-04-16 18:11:19 +0200

    ws test

--- index.js
+++ index.js
@@ -137,6 +137,8 @@ var wss = new WebSocketServer({server: server});
 
 wss.on('connection', function(socket) {
   stream.on('tweet', function(tweet) {
-    socket.send(tweet)
+    socket.send(tweet, function(err) {
+      console.log(err)
+    })
   })
 })
